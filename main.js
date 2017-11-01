8b96b31fceb5731439d92ec6c791800d6ba5bcc4
John Kasarda <jkasar@saic.edu>
2017-10-31 23:39:36 -0500

    second iteration on portraying concept

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
 var width = innerWidth / 2;
 var height = innerHeight / 2;
+var art = ["I", " ", "am", " ", "making", " ", "art." ];
+var startPlace = 0;
 
 
 
@@ -25,18 +27,22 @@ function resizeCanvas(){
 
 function makingArt(){ 
 
-return 'I am making art.'}
+return 'I am making art.';
 
+}
 
- var x = setInterval( function(){ 
+var x = setInterval( function(){ 
 
- console.log(makingArt());}, 3000);
+ console.log(makingArt());
+
+}, 3000);
 
 /* ----------------------------------------------
 				~event listeners~
 ------------------------------------------------*/
 
 window.addEventListener("resize", resizeCanvas );
+
 //window.addEventListener("mousemove", updateMouseXY );
 
 resizeCanvas();
@@ -46,24 +52,67 @@ resizeCanvas();
 -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_*/
 
 function draw(){
-	ctx.fillStyle = 'rgb(0,0,0,.1)';
-	ctx.fillRect(0,0, canvas.width, canvas.height);
+	//ctx.fillStyle = 'rgb(255,255,255, 0)';
+	//ctx.fillRect(0,0, canvas.width, canvas.height);
+
+
+	//for (var i = art.length - 1; i >= 0; i--) {
+	
+
+	// ctx.fillStyle = "#00FF00";
+	// ctx.strokeStyle = "#00FF00";
+	// ctx.font = " 100px sans-serif";
+	// ctx.lineWidth = ".5";
+	// ctx.fillText(art[i], width + 1 , height);
+	// ctx.strokeText(art[i], width + 1, height); 
+	// };
+	
+	setInterval( function(){ 
 
 	ctx.fillStyle = "#00FF00";
 	ctx.strokeStyle = "#00FF00";
 	ctx.font = " 100px serif";
 	ctx.lineWidth = ".5";
-	setInterval( function(){ 
-		ctx.fillText(makingArt(), 100, 100 );
-	ctx.strokeText(makingArt(), 100, 100); 
+	ctx.fillText(art[0], 100, 100 );
+	ctx.strokeText(art[0], 100, 100); 
 
- ;}, 500);
+ 	;}, 200);
+ 	setInterval( function(){ 
 
-	setInterval( function(){ 
-		cctx.fillStyle = 'rgb(0,0,0,.1)';
-	ctx.fillRect(0,0, canvas.width, canvas.height);
+	ctx.fillStyle = "#00FF00";
+	ctx.strokeStyle = "#00FF00";
+	ctx.font = " 100px serif";
+	ctx.lineWidth = ".5";
+	ctx.fillText(art[1], 200, 100 );
+	ctx.strokeText(art[1], 200, 100); 
 
- ;}, 1000);
+ 	;}, 400);
+ 		setInterval( function(){ 
+
+	ctx.fillStyle = "#00FF00";
+	ctx.strokeStyle = "#00FF00";
+	ctx.font = " 100px serif";
+	ctx.lineWidth = ".5";
+	ctx.fillText(art[2], 300, 100 );
+	ctx.strokeText(art[2], 300, 100); 
+
+ 	;}, 600);
+ 	setInterval( function(){ 
+
+	ctx.fillStyle = "#00FF00";
+	ctx.strokeStyle = "#00FF00";
+	ctx.font = " 100px serif";
+	ctx.lineWidth = ".5";
+	ctx.fillText(art[3], 400, 100 );
+	ctx.strokeText(art[3], 400, 100); 
+
+ 	;}, 800);
+
+	// setInterval( function(){ 
+	// 	ctx.fillStyle = 'rgb(0,0,0,.1)';
+	// ctx.fillRect(0,0, canvas.width, canvas.height);
+
+ // ;}, 1000);
 
 	
 	requestAnimationFrame(draw); //allows for maximum use of the hardwares potential
